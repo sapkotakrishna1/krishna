97af84a1eb86c67bb9607ec22cc79653a16acf7d
sapkotakrishna1 <99406544+sapkotakrishna1@users.noreply.github.com>
2022-05-12 18:20:45 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*------------typing animation----------------*/
+var typed= new Typed(".typing",{
+    strings:["","web designer","Web developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
